67c3735160e43eb64844e0294f2372e400aad38f
abdul-rehman-ship <abd22655@gmail.com>
2024-08-29 22:29:38 +0500

    first commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ admin.initializeApp({
         privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       }),
-    // credential: admin.credential.applicationDefault(),
+    //   databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
     
 });
 
